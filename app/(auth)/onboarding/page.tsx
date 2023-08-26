011fc6124005a153386a16fa58344db25fdc560b
Tanishka-dev <tanishakayadav2110@gmail.com>
2023-08-27 00:20:35 +0530

    IDK

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,3 +1,5 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+
 function Page() {
   return (
     <main className="mx-auto px-10 py-20 flex max-w-3xl flex-col justify-start">
@@ -5,7 +7,9 @@ function Page() {
       <p className="mt-3 text-base-regular text-[#c4c4c8]">
         Share Challenges, Spark Solutions, and Elevate Your Expertise in Tech.
       </p>
-      <section className="mt-9 p-10 bg-[#1E293B]"></section>
+      <section className="mt-9 p-10 bg-[#1E293B]">
+        <AccountProfile />
+      </section>
     </main>
   );
 }
---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const AccountProfile = () => {
+  return <div></div>;
+};
+
+export default AccountProfile;
